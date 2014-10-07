b43e31940b85b5407aaeda80b7c9071efe520c89
amoAHCP <amo.ahcp@gmail.com>
2014-10-07 21:29:49 +0200

    initial commit packaging demo

---
+++ UeberJarUnpackDep/src/main/java/org/jacpfx/demo/maven/SimpleHandler.java
@@ -0,0 +1,50 @@
+/*
+ * **********************************************************************
+ *
+ *  Copyright (C) 2010 - 2014
+ *
+ *  [Component.java]
+ *  JACPFX Project (https://github.com/JacpFX/JacpFX/)
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ *
+ * *********************************************************************
+ */
+
+package org.jacpfx.demo.maven;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by amo on 07.10.14.
+ */
+public class SimpleHandler extends AbstractHandler
+{
+    public void handle(String target,Request baseRequest,HttpServletRequest request,HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+}
